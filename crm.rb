37b662de9e03ae391a0bd8a1e6366671ed218564
BlackDiscGH <avinashjham@Avinashs-MacBook-Pro.local>
2015-11-03 15:29:38 -0500

    Exercise 3 completed

--- crm.rb
+++ crm.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
 get '/' do
-  erb :index
+	@crm_app_name = "My CRM"
+	@time = Time.now
+  	erb :index
 end
+
+get '/contacts' do
+	@crm_app_name = "MY CONTACTS"
+  	erb :contacts
+end
+
+get '/contacts/new' do
+	@crm_app_name = "MY CONTACTS"
+  	erb :new
+end
+
