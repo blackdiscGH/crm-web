d218ea0f6bd560f4e911c1ab3c344c21a16d2f95
BlackDiscGH <avinashjham@Avinashs-MacBook-Pro.local>
2015-11-06 13:15:42 -0500

    Improving CRM-WEB

--- crm.rb
+++ crm.rb
@@ -1,9 +1,20 @@
 require 'sinatra'
-require_relative 'contact'
 require 'date'
 require 'data_mapper'
 
 DataMapper.setup(:default, "sqlite3:database.sqlite3")
+
+class Contact
+  attr_accessor :id, :first_name, :last_name, :email, :note
+
+  def initialize(first_name, last_name, email, note) #............................................................+
+      @first_name = first_name
+      @last_name = last_name
+      @email = email
+      @note = note
+  end #............................................................................................................-
+end 
+
 
 Contact.create("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar")
 
