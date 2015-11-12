111e88299c0aea188a7232dfd3e66e65097107de
BlackDiscGH <avinashjham@Avinashs-MacBook-Pro.local>
2015-11-12 15:15:04 -0500

    Final Changes

--- crm.rb
+++ crm.rb
@@ -11,21 +11,11 @@ class Contact
     property :first_name, String
     property :last_name, String
     property :email, String
-    property :note, String
-  # def initialize(first_name, last_name, email, note) #............................................................+
-  #     @first_name = first_name
-  #     @last_name = last_name
-  #     @email = email
-  #     @note = note
-  # end #............................................................................................................-
-
+    property :notes, String
 DataMapper.finalize
 DataMapper.auto_upgrade!
-
 end 
 
-
-Contact.create("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar")
 
 get '/' do   #Show the Index page -----------------------------------------------------------
 	@crm_app_name = "My CRM"
@@ -33,14 +23,12 @@ get '/' do   #Show the Index page ----------------------------------------------
   	erb :index
 end
 
-
 get "/contacts/new" do
 erb :new
 end
 
-
 get "/contacts/:id" do  # Get the detailes page of a contact --------------------------------
-  @contact = Contact.find(params[:id].to_i)
+  @contact = Contact.get(params[:id].to_i)
   if @contact 
     erb :show_contact
   else
@@ -48,24 +36,23 @@ get "/contacts/:id" do  # Get the detailes page of a contact -------------------
   end 
 end
 
-
-get '/contacts' do
-	   @crm_app_name = "MY CONTACTS"
-	  # Contact.create("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  	# Contact.create("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  	# Contact.create("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-  	erb :contacts
+get '/contacts' do  # Display all the contacts -----------------------------------------------
+	   @contacts = Contact.all
+  	 erb :contacts
 end
 
-
-post '/contacts' do
-  #puts params
-  Contact.create(params[:first_name], params[:last_name], params[:email], params[:notes])
+post '/contacts' do # Receiver for posting a new contact using data_mapper -------------------
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :notes => params[:notes]
+    )
   redirect to('/contacts')
 end
 
-get "/contacts/:id/edit" do
-  @contact = Contact.find(params[:id].to_i)
+get "/contacts/:id/edit" do # Retreiving the contact using data mapper and passing @contact to Edit_Contact page   -----------
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     erb :edit_contact
   else
@@ -74,13 +61,15 @@ get "/contacts/:id/edit" do
 
 end
 
-put "/contacts/:id" do
-  @contact = Contact.find(params[:id].to_i)
+put "/contacts/:id" do # T his is to update
+  @contact = Contact.get(params[:id].to_i)
   if @contact
     @contact.first_name = params[:first_name]
     @contact.last_name = params[:last_name]
     @contact.email = params[:email]
     @contact.notes = params[:notes]
+
+    @contact.save
 
     redirect to("/contacts")
   else
@@ -90,12 +79,11 @@ end
 
 delete "/contacts/:id" do
 
-    @contact = Contact.find(params[:id].to_i)
-    if @contact
-       @contact.remove
+    contact = Contact.get(params[:id].to_i)
+    if contact
+       contact.destroy
        redirect to("/contacts")
     else
-   
        raise Sinatra::NotFound
     end 
 
